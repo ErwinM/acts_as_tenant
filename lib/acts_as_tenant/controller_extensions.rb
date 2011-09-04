8ba863c7f5dc84fcbaa76bbe99c2c4582054194a
Erwin Matthijssen <erwin.matthijssen@gmail.com>
2011-09-04 22:44:53 +0200

    Fix to get current_tenant working3

--- lib/acts_as_tenant/controller_extensions.rb
+++ lib/acts_as_tenant/controller_extensions.rb
@@ -1,52 +1,48 @@
 module ActsAsTenant
   module ControllerExtensions
-    extend ActiveSupport::Concern
       
-    included do
-      helper_method :current_tenant
+    helper_method :current_tenant
+    
+  
+    
+    # this method allows setting the current_account by reading the subdomain and looking
+    # it up in the tenant-model passed to the method (defaults to Account). The method will 
+    # look for the subdomain in a column referenced by the second argument (defaults to subdomain).
+    def set_current_tenant_by_subdomain(tenant = :account, column = :subdomain )
+      self.class_eval do
+        cattr_accessor :tenant_class, :tenant_column
+        attr_accessor :current_tenant
+      end
+
+      self.tenant_class = tenant.to_s.capitalize.constantize
+      self.tenant_column = column.to_sym
+
+      self.class_eval do
+        before_filter :find_tenant_by_subdomain
+
+        private
+          def find_tenant_by_subdomain
+            ActsAsTenant.current_tenant = tenant_class.where(tenant_column => request.subdomains.first).first
+            @current_tenant_instance = ActsAsTenant.current_tenant
+          end
+      end
     end
-  
-    module ClassMethods
-      
-      # this method allows setting the current_account by reading the subdomain and looking
-      # it up in the tenant-model passed to the method (defaults to Account). The method will 
-      # look for the subdomain in a column referenced by the second argument (defaults to subdomain).
-      def set_current_tenant_by_subdomain(tenant = :account, column = :subdomain )
-        self.class_eval do
-          cattr_accessor :tenant_class, :tenant_column
-          attr_accessor :current_tenant
-        end
-  
-        self.tenant_class = tenant.to_s.capitalize.constantize
-        self.tenant_column = column.to_sym
-  
-        self.class_eval do
-          before_filter :find_tenant_by_subdomain
 
-          private
-            def find_tenant_by_subdomain
-              ActsAsTenant.current_tenant = tenant_class.where(tenant_column => request.subdomains.first).first
-              @current_tenant_instance = ActsAsTenant.current_tenant
-            end
-        end
+    def set_current_tenant_to(current_tenant_object)
+      self.class_eval do
+        cattr_accessor :tenant_class
+        attr_accessor :current_tenant
+        before_filter lambda { @current_tenant_instance = ActsAsTenant.current_tenant = current_tenant_object }
       end
+    end
 
-      def set_current_tenant_to(current_tenant_object)
-        self.class_eval do
-          cattr_accessor :tenant_class
-          attr_accessor :current_tenant
-          before_filter lambda { @current_tenant_instance = ActsAsTenant.current_tenant = current_tenant_object }
-        end
-      end
+    # helper method to have the current_tenant available in the controller  
+    def current_tenant
+      @current_tenant_instance
+    end
 
-      # helper method to have the current_tenant available in the controller  
-      def current_tenant
-        ActsAsTenant.current_tenant
-      end
-  
-      ActiveSupport.on_load(:action_controller) do
-        helper_method :current_tenant
-      end
+    ActiveSupport.on_load(:action_controller) do
+      helper_method :current_tenant
     end
   end
 end
