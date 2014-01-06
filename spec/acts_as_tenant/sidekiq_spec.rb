ffdda064e7f22f591871d3c930f47a045f0c1612
Tobias Maier <info@tobiasmaier.info>
2014-01-06 21:46:52 +0100

    Add specs for sidekiq

---
+++ spec/acts_as_tenant/sidekiq_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+require 'sidekiq'
+require 'acts_as_tenant/sidekiq'
+
+class Account
+  def id
+    1234
+  end
+end
+
+describe ActsAsTenant::Sidekiq do
+  after { ActsAsTenant.current_tenant = nil }
+  let(:message) { { 'acts_as_tenant' => { 'class' => 'Account', 'id' => 1234 } } }
+
+  describe ActsAsTenant::Sidekiq::Client do
+    subject { ActsAsTenant::Sidekiq::Client.new }
+
+    it 'saves tenant if present' do
+      ActsAsTenant.current_tenant = Account.new
+
+      msg = {}
+      subject.call(nil, msg, nil) { }
+      expect(msg).to eq message
+    end
+
+    it 'does not set tenant if not present' do
+      expect(ActsAsTenant.current_tenant).to be_nil
+
+      msg = {}
+      subject.call(nil, msg, nil) { }
+      expect(msg).not_to eq message
+    end
+  end
+
+  describe ActsAsTenant::Sidekiq::Server do
+    subject { ActsAsTenant::Sidekiq::Server.new }
+
+    it 'restores tenant if tenant saved' do
+      expect(Account).to receive(:find).with(1234).once { Account.new }
+
+      msg = message
+      subject.call(nil, msg, nil) do
+        expect(ActsAsTenant.current_tenant).to be_a_kind_of Account
+      end
+      expect(ActsAsTenant.current_tenant).to be_nil
+    end
+
+    it 'runs without tenant if no tenant saved' do
+      expect(Account).not_to receive(:find)
+
+      msg = {}
+      subject.call(nil, msg, nil) do
+        expect(ActsAsTenant.current_tenant).to be_nil
+      end
+      expect(ActsAsTenant.current_tenant).to be_nil
+    end
+  end
+end
