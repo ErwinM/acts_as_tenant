a96bf528663958db696144cc8295495ecf45492e
Christian <preth00nker@gmail.com>
2014-01-27 10:04:04 -0600

    added set_current_tenant_by_subdomain_or_domain

--- lib/acts_as_tenant/controller_extensions.rb
+++ lib/acts_as_tenant/controller_extensions.rb
@@ -26,6 +26,34 @@ module ActsAsTenant
           end
       end
     end
+
+    # 01/27/2014 Christian Yerena / @preth00nker
+    # this method adds the possibility of use the domain as a possible second argument to find 
+    # the current_tenant. 
+    def set_current_tenant_by_subdomain_or_domain(tenant = :account, primary_column = :subdomain, second_column = :domain )
+      self.class_eval do
+        cattr_accessor :tenant_class, :tenant_primary_column, :tenant_second_column
+      end
+      
+      self.tenant_class = tenant.to_s.camelcase.constantize
+      self.tenant_primary_column = primary_column.to_sym
+      self.tenant_second_column = second_column.to_sym
+      
+      self.class_eval do
+        before_filter :find_tenant_by_subdomain_or_domain
+        helper_method :current_tenant
+        
+        private
+          def find_tenant_by_subdomain_or_domain
+            ActsAsTenant.current_tenant = tenant_class.where(tenant_primary_column => request.subdomains.last).first || tenant_class.where(tenant_second_column => request.domain).first
+          end
+          
+          def current_tenant
+            ActsAsTenant.current_tenant
+          end
+      end
+    end
+
     
     # This method sets up a method that allows manual setting of the current_tenant. This method should
     # be used in a before_filter. In addition, a helper is setup that returns the current_tenant
