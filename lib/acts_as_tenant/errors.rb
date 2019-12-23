c419f81438f08f6bade1252e571a497144472728
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-23 23:02:55 +0200

    Cleanup (#209)
    * Convert dos style end of line to unix one
    
    * Strip whitespaces

--- lib/acts_as_tenant/errors.rb
+++ lib/acts_as_tenant/errors.rb
@@ -5,15 +5,15 @@ module ActsAsTenant
   module Errors
     class ModelNotScopedByTenant < ActsAsTenant::Error
     end
-    
+
     class NoTenantSet < ActsAsTenant::Error
     end
-    
+
     class ModelNotScopedByTenant < ActsAsTenant::Error
     end
-    
+
     class TenantIsImmutable < ActsAsTenant::Error
     end
-    
+
   end
 end
