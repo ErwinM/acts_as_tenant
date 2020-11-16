62c117c725b3098135118c4ea06f7f13460b400c
Chris Oliver <excid3@gmail.com>
2020-11-16 10:53:46 -0600

    Refactor view helper spec to test against ActionView::Base

--- spec/helpers/tenant_helper_spec.rb
+++ spec/helpers/tenant_helper_spec.rb
@@ -1,14 +1,16 @@
 require "spec_helper"
 
-describe ActsAsTenant::TenantHelper, type: :helper do
-  describe "#current_tenant" do
-    it "returns nil if no tenant set" do
-      expect(helper.current_tenant).to be_nil
-    end
+describe ActionView::Base, type: :helper do
+  it "responds ot current_tenant" do
+    expect(helper).to respond_to(:current_tenant)
+  end
 
-    it "returns nil if no tenant set" do
-      ActsAsTenant.current_tenant = accounts(:foo)
-      expect(helper.current_tenant).to eq(accounts(:foo))
-    end
+  it "returns nil if no tenant set" do
+    expect(helper.current_tenant).to be_nil
+  end
+
+  it "returns the current tenant" do
+    ActsAsTenant.current_tenant = accounts(:foo)
+    expect(helper.current_tenant).to eq(accounts(:foo))
   end
 end
