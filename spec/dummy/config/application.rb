7bb64f847063026b07caad3c29d9c26e83b800a4
Chris Oliver <excid3@gmail.com>
2020-11-17 17:06:08 -0600

    Fix sqlite deprecation warning for Rails <6

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -11,5 +11,9 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    if Rails.gem_version < Gem::Version.new("6.0") && config.active_record.sqlite3
+      config.active_record.sqlite3.represent_boolean_as_integer = true
+    end
   end
 end
