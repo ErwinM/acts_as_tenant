0b8933ea1fed938c26f8a3ceac23364191620c41
Cody Krieger <cody@krieger.io>
2023-01-21 11:26:30 -0500

    allow 'touch' option to be passed through to belongs_to association (#306)

--- lib/acts_as_tenant/model_extensions.rb
+++ lib/acts_as_tenant/model_extensions.rb
@@ -10,7 +10,7 @@ module ActsAsTenant
         ActsAsTenant.add_global_record_model(self) if options[:has_global_records]
 
         # Create the association
-        valid_options = options.slice(:foreign_key, :class_name, :inverse_of, :optional, :primary_key, :counter_cache, :polymorphic)
+        valid_options = options.slice(:foreign_key, :class_name, :inverse_of, :optional, :primary_key, :counter_cache, :polymorphic, :touch)
         fkey = valid_options[:foreign_key] || ActsAsTenant.fkey
         pkey = valid_options[:primary_key] || ActsAsTenant.pkey
         polymorphic_type = valid_options[:foreign_type] || ActsAsTenant.polymorphic_type
